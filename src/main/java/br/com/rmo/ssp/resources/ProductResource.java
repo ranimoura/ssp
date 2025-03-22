85470e2ec851b664d6eef6977d6ecd5f828cf724
ranimoura <raniereoliveira@gmail.com>
2025-03-22 18:18:22 -0300

    Ajustes nas classes ProductService e ProductResource para melhorar o Pageable.

--- src/main/java/br/com/rmo/ssp/resources/ProductResource.java
+++ src/main/java/br/com/rmo/ssp/resources/ProductResource.java
@@ -4,8 +4,7 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -28,14 +26,12 @@ public class ProductResource {
 	@Autowired
 	private ProductService service;
 
+	// O OBJETO PAGEABLE REPASSA 3 PROPRIEDADES IMPORTANTES: PAGE, SIZE E SORT.
+	// ESSAS PROPRIEDADES SÃO AS MESMAS MANIPULADAS NA CLASSE CATEGORY MANUALMENTE.
 	@GetMapping
-	public ResponseEntity<Page<ProductDTO>> findAll(@RequestParam(defaultValue = "0") Integer page,
-			@RequestParam(defaultValue = "5") Integer linesPerPage,
-			@RequestParam(defaultValue = "ASC") String direction, @RequestParam(defaultValue = "name") String orderBy) {
-
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-
-		Page<ProductDTO> list = service.findAllPaged(pageRequest);
+	public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
+		
+		Page<ProductDTO> list = service.findAllPaged(pageable);
 
 		return ResponseEntity.ok(list);
 	}
